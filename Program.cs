16e08de79bf14d0a40b125577ec8c28e63cd191f
Murilo Kauan Socek <socekmurilo@gmail.com>
2023-10-27 16:56:03 -0300

    Assincrona

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Syste.Net.Http;
+
+HtppClient client = new HtppClient();
+
+var url = "https://www.bosch.com.br/";
+
+var reponsive = await client.GetAsync(url);
+
+
+
+File.WriteAllText("", reponsive.)
+
+
+// desafio: pedir  a url, ao ele passar o sistema pergunta se ele quer salvar ou nao, caso sim, criar um .html, caso nao, printar o html
