93ed4ad2cbd7a6e1d8514d9b60c16e83ec144c86
Murilo Kauan Socek <socekmurilo@gmail.com>
2023-10-30 13:28:17 -0300

    Baixando Site

--- Program.cs
+++ Program.cs
@@ -1,15 +1,28 @@
 ﻿using System.IO;
-using Syste.Net.Http;
+using System.Net.Http;
+using System;
+using Microsoft.VisualBasic;
 
-HtppClient client = new HtppClient();
+HttpClient client = new HttpClient();
 
-var url = "https://www.bosch.com.br/";
+// var url = "https://www.bosch.com.br/";
 
-var reponsive = await client.GetAsync(url);
+Console.WriteLine("Digite o Site que deseja baixar");
+var url = Console.ReadLine();
 
-
-
-File.WriteAllText("", reponsive.)
+var responsive = await client.GetAsync(url);
+if (responsive.StatusCode == 0)
+    Console.WriteLine("Nao foi encontrado");
+else
+{
+    var html = await responsive.Content.ReadAsStringAsync();
+    Console.WriteLine("Deseja baixar: (1 -> sim  2 -> não)");
+    var choose = Console.ReadLine();
+    if (choose == "1")
+    {
+       await File.WriteAllTextAsync("Site.html", html);
+    }
+}
 
 
 // desafio: pedir  a url, ao ele passar o sistema pergunta se ele quer salvar ou nao, caso sim, criar um .html, caso nao, printar o html
